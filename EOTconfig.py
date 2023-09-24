a07d41e4524acf7b8e0d4e99045b2d7ee09a1edb
Russ Innes <russ.innes@gmail.com>
2023-09-24 12:55:04 -0600

    Update EOTconfig.py

--- EOTconfig.py
+++ EOTconfig.py
@@ -5,8 +5,9 @@ Config parameters for EOTdecode.py
 # Set to True if using an RTL SDR dongle for RF input, False if using soundcard input
 USE_RTL = True
 
-# RTL Frequency
+# RTL EOT device Frequency
 FREQ = '457.9375M'
+
 # RTL Device Number, usually 0 if only a single device is plugged in.
 DEVICE = 0
 
