078f44f0750e7e339306d146b433687d2ed1c5d3
Russ Innes <russ.innes@gmail.com>
2023-09-24 12:40:37 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo apt update
 echo "====== Installing required build tools"
 sudo apt install git cmake pkg-config libusb-1.0-0-dev sox
 sudo apt install build-essential automake autoconf
-sudo apt nstall libsndfile1-dev fftw3-dev libasound2-dev libpulse-dev libsndio-dev
+sudo apt install libsndfile1-dev fftw3-dev libfftw3-dev libasound2-dev libpulse-dev libsndio-dev
 
 echo "====== Checking for RTL_FM"
 
